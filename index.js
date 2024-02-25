e5d27fbc9bad98798b0e30a61d2fea6735ee4db1
Bradley Troll <bradleytroll@gmail.com>
2024-02-25 10:23:59 -0500

    Adds array of questions.

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// original code: const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project, explaining the what, hwy, and how of your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Type the directions for installing your project',
+        },
+        {
+            type: 'input',
+            name: 'uage',
+            message: 'Type the instructions for using your application',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List any collaborators on this project as well as their GitHub profile',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Include your license information or put None.',
+        },
+    ]);
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
